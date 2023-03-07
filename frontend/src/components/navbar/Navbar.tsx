b0b889fde3b6c2f02ce14f6487270fc1c04b0f67
Kristoffer Larsen Hopland <24739853+BM0B0T@users.noreply.github.com>
2023-03-07 18:38:26 +0100

    added navbar and router

---
+++ frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,50 @@
+import MenuIcon from '@mui/icons-material/Menu'
+import HomeIcon from '@mui/icons-material/Home'
+import SearchIcon from '@mui/icons-material/search'
+import PersonIcon from '@mui/icons-material/AccountCircle'
+import Drawer from '@mui/material/Drawer'
+import { List, ListItem, ListItemButton, ListItemText } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
+export function NavBar(props: {}) {
+  const navigate = useNavigate()
+
+  return (
+    <Drawer variant="permanent">
+      <List className="w-20">
+        <ListItem>
+          <ListItemButton className="flex flex-col">
+            <MenuIcon />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton
+            className="flex flex-col"
+            onClick={(_) => navigate('/')}
+          >
+            <HomeIcon />
+            <ListItemText>home</ListItemText>
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton
+            className="flex flex-col"
+            onClick={(_) => navigate('/hi')}
+          >
+            <SearchIcon />
+            <ListItemText>discover</ListItemText>
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton
+            className="flex flex-col"
+            onClick={(_) => navigate('/hi2')}
+          >
+            <PersonIcon />
+            <ListItemText>profile</ListItemText>
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Drawer>
+  )
+}
