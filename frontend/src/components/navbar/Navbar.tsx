79dcc9f76781729d7c540e80b7e0cc2e36f6a8ff
Deogratias Djuma Saidi <50410476+deogratias-saidi@users.noreply.github.com>
2023-03-16 17:50:31 +0100

    add media query for mobile (#16)

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -3,48 +3,102 @@ import HomeIcon from '@mui/icons-material/Home'
 import SearchIcon from '@mui/icons-material/Search'
 import PersonIcon from '@mui/icons-material/AccountCircle'
 import Drawer from '@mui/material/Drawer'
-import { List, ListItem, ListItemButton, ListItemText } from '@mui/material'
+
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  useMediaQuery,
+} from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 
 export function NavBar(props: {}) {
   const navigate = useNavigate()
 
+  const isMobile = useMediaQuery('(max-width: 600px)')
+
   return (
-    <Drawer variant="permanent">
-      <List className="w-20">
-        <ListItem>
-          <ListItemButton className="flex flex-col">
-            <MenuIcon />
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton
-            className="flex flex-col"
-            onClick={(_) => navigate('/')}
-          >
-            <HomeIcon />
-            <ListItemText>home</ListItemText>
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton
-            className="flex flex-col"
-            onClick={(_) => navigate('/hi')}
-          >
-            <SearchIcon />
-            <ListItemText>discover</ListItemText>
-          </ListItemButton>
-        </ListItem>
-        <ListItem>
-          <ListItemButton
-            className="flex flex-col"
-            onClick={(_) => navigate('/hi2')}
-          >
-            <PersonIcon />
-            <ListItemText>profile</ListItemText>
-          </ListItemButton>
-        </ListItem>
-      </List>
-    </Drawer>
+    <>
+      {isMobile ? (
+        <>
+          <Drawer variant="permanent" anchor="top">
+            <List className="w-screen flex justify-between h-16">
+              <ListItem>
+                <ListItemButton className="flex flex-col">
+                  <MenuIcon
+                  // When I click on the menu icon, I want the menu to open
+                  />
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton
+                  className="flex flex-col"
+                  onClick={(_) => navigate('/')}
+                >
+                  <HomeIcon />
+               
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton
+                  className="flex flex-col"
+                  onClick={(_) => navigate('/hi')}
+                >
+                  <SearchIcon />
+                  <ListItemText></ListItemText>
+                </ListItemButton>
+              </ListItem>
+              <ListItem>
+                <ListItemButton
+                  className="flex flex-col"
+                  onClick={(_) => navigate('/hi2')}
+                >
+                  <PersonIcon />
+                  
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Drawer>
+        </>
+      ) : (
+        <Drawer variant="permanent">
+          <List className="w-20">
+            <ListItem>
+              <ListItemButton className="flex flex-col">
+                <MenuIcon />
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton
+                className="flex flex-col"
+                onClick={(_) => navigate('/')}
+              >
+                <HomeIcon />
+                
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton
+                className="flex flex-col"
+                onClick={(_) => navigate('/hi')}
+              >
+                <SearchIcon />
+                <ListItemText>discover</ListItemText>
+              </ListItemButton>
+            </ListItem>
+            <ListItem>
+              <ListItemButton
+                className="flex flex-col"
+                onClick={(_) => navigate('/hi2')}
+              >
+                <PersonIcon />
+                <ListItemText>profile</ListItemText>
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Drawer>
+      )}
+    </>
   )
 }
