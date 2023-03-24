5c2e628005d63fe3f7ee4b520bd3bb633c0f50db
Trym Valebjorg <trym.valebjorg@avanade.com>
2023-03-24 18:02:13 +0100

    Refactor navrail with beercss, separated routes to pages

---
+++ frontend/src/pages/ExplorePage.tsx
@@ -0,0 +1,13 @@
+// Create a basic home page component with a div that says "Home Page"
+import React from 'react';
+import 'beercss';
+
+export const ExplorePage: React.FC = () => {
+  return (
+    <main className="responsive bg-surface">
+        <h1>Explore Page</h1>
+    </main>
+  );
+};
+
+export default ExplorePage;
---
+++ frontend/src/pages/ProfilePage.tsx
@@ -0,0 +1,13 @@
+// Create a basic home page component with a div that says "Home Page"
+import React from 'react';
+import 'beercss';
+
+export const ProfilePage: React.FC = () => {
+  return (
+    <main className="responsive bg-surface">
+        <h1>Profile Page</h1>
+    </main>
+  );
+};
+
+export default ProfilePage;
