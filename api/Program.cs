ccc0fd265987a75afca055ddc1b85dab036b0d4b
Kristoffer Larsen Hopland <24739853+BM0B0T@users.noreply.github.com>
2023-03-25 22:57:22 +0100

    fix timespan in swagger

--- api/Program.cs
+++ api/Program.cs
@@ -4,12 +4,16 @@ using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>{
+    c.MapType<TimeSpan>(() => new OpenApiSchema { Type = "string",  Example = new OpenApiString("00:00:00") });
+});
 builder.Services.AddSpaStaticFiles(c => { c.RootPath = "dist"; });
 builder.Services.AddMediator(o => { o.ServiceLifetime = ServiceLifetime.Scoped; });
 builder.Services.AddDbContext<BachelorDbContext>(o =>
