4513de1e1c87f60369caeb853ab240f1483935bb
Trym Valebjorg <trym.valebjorg@avanade.com>
2023-03-27 13:27:11 +0200

    Rename to 'discover', replacing 'explore'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { NavRail } from './components/NavRail/NavRail';
 import { NavItem } from './components/NavRail/NavItem';
 import { HomePage } from './pages/HomePage';
-import { ExplorePage } from './pages/ExplorePage';
+import { DiscoverPage } from './pages/DiscoverPage';
 import { ProfilePage } from './pages/ProfilePage';
 import CoursePage from './pages/CoursePage';
 
@@ -10,12 +10,12 @@ const App: React.FC = () => (
   <BrowserRouter>
     <NavRail>
       <NavItem icon="home" label="Home" route="/" />
-      <NavItem icon="search" label="Explore" route="/explore" />
+      <NavItem icon="search" label="Discover" route="/discover" />
       <NavItem icon="account_circle" label="Profile" route="/profile" />
     </NavRail>
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/explore" element={<ExplorePage />} />
+      <Route path="/discover" element={<DiscoverPage />} />
       <Route path="/profile" element={<ProfilePage />} />
       <Route path="/course" element={<CoursePage />} />
     </Routes>
--- frontend/src/pages/DiscoverPage.tsx
+++ frontend/src/pages/DiscoverPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import DiscoverHeader from '../components/Discover/Header';
 import FilterMenu from '../components/Discover/FilterMenu'
 import FilterItems from '../components/Discover/FiltereItems'
@@ -10,7 +9,7 @@ import 'beercss';
 const title = 'Discover new courses'
 const description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ducimus distinctio, maiores, vel suscipit neque animi velit natus libero exercitationem, dignissimos omnis perferendis voluptate sed autem. In eaque excepturi dolorem.'
 
-export const ExplorePage: React.FC = () => {
+export const DiscoverPage: React.FC = () => {
   return (
     <main className="ml-[5rem] p-5 grid grid-cols-7 gap-5 auto-rows-max h-[99vh] w-100 relative">
 
