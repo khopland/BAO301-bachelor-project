11ef51ec3cee4ad28d86b4c80f9e9dc5d58c75f4
Kristoffer Larsen Hopland <24739853+BM0B0T@users.noreply.github.com>
2023-03-27 08:12:14 +0200

    Feature/better docker (#23)
    * added better docker files
    
    * fix timespan in swagger
    
    * better swagger ui

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # BAO301-bachelor-project
+
+# How to run the app
+# requirements
+- [dotnet 7 sdk](https://dotnet.microsoft.com/en-us/download)
+- [Node 18 or greater](https://nodejs.org/en)
+- [Docker](https://www.docker.com/products/docker-desktop/)
+
+## DEV
+### Frontend
+- have `node` installed
+- `cd` in to frontend folder
+- run `npm install` if not done it already
+- run `npm run dev` to start the frontend server on port 3000
+### Backend
+- have dotnet 7 sdk installed
+- `cd` in to api folder
+- run `dotnet run` to build and start the backend on port http:5000 or https:5001
+in dev it has a proxy for the frontend so you need to run both to make it all work and go to port 5000 or 5001.
+## Database
+you need to have docker installed or a postgres database running on your system for the backend to work.
+###### Docker DB
+run `docker compose --file .\docker-compose-db.yml up` for postgres on a docker image.
+
+
+## Docker
+need to have docker installed.
+- run `docker compose up` to build and run the app with bot frontend, backend and DB in 1 command.
+
+
+## Production 
+To run this in production you need to set the ENV postgresConnectionString to the postgres connection string.
+- build the docker image with `docker build -t BAO .` and upload it to a server or a container registry.
--- api/Program.cs
+++ api/Program.cs
@@ -1,15 +1,20 @@
 using api;
 using api.Requests;
 using Core.Interfaces;
+using Core.Models;
 using Infrastructure.Data;
 using Infrastructure.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>{
+    c.MapType<TimeSpan>(() => new OpenApiSchema { Type = "string",  Example = new OpenApiString("00:00:00") });
+});
 builder.Services.AddSpaStaticFiles(c => { c.RootPath = "dist"; });
 builder.Services.AddMediator(o => { o.ServiceLifetime = ServiceLifetime.Scoped; });
 builder.Services.AddDbContext<BachelorDbContext>(o =>
@@ -23,6 +28,14 @@ builder.Services.AddScoped<ISkillRepository, SkillRepository>();
 builder.Services.AddScoped<ICourseTypeRepository, CourseTypeRepository>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<BachelorDbContext>();
+    if (context.Database.GetPendingMigrations().Any())
+        context.Database.Migrate();
+}
+
 app.UseSpaStaticFiles();
 app.MapWhen(x => !((x.Request.Path.Value?.StartsWith("/api") ?? false)
                  || (x.Request.Path.Value?.StartsWith("/swagger") ?? false)), b =>
@@ -40,24 +53,24 @@ var group = app.MapGroup("/api");
 
 group.MapGet("/hello", () => Results.Json(new { text = "Hello from server!!" }));
 
-group.MediateGet<GetUserRequest>("/user/{userId:guid}");
-group.MediateGet<GetAllUsersRequest>("/user");
-group.MediatePost<CreatUserRequest>("/user");
+group.MediateGet<GetUserRequest>("/user/{userId:guid}","User",typeof(User));
+group.MediateGet<GetAllUsersRequest>("/user","User",typeof(List<User>));
+group.MediatePost<CreatUserRequest>("/user","User",typeof(User));
 
-group.MediateGet<GetCategoriesRequest>("/Category");
-group.MediatePost<CreateCategoryRequest>("/Category");
+group.MediateGet<GetCategoriesRequest>("/category","Category",typeof(List<Category>));
+group.MediatePost<CreateCategoryRequest>("/category","Category",typeof(Category));
 
-group.MediateGet<GetTypesRequest>("/type");
-group.MediatePost<CreateTypeRequest>("/type");
+group.MediateGet<GetTypesRequest>("/type","Type",typeof(List<CourseType>));
+group.MediatePost<CreateTypeRequest>("/type","Type",typeof(CourseType));
 
-group.MediateGet<GetSkillsRequest>("/skill");
-group.MediatePost<CreateSkillRequest>("/skill");
+group.MediateGet<GetSkillsRequest>("/skill","Skill",typeof(List<Skill>));
+group.MediatePost<CreateSkillRequest>("/skill","Skill",typeof(Skill));
 
-group.MediateGet<GetAllLanguagesRequest>("/language");
+group.MediateGet<GetAllLanguagesRequest>("/language","Language",typeof(List<string>));
 
-group.MediateGet<GetCourseRequest>("/course/{courseId:guid}");
-group.MediateGet<GetAllCoursesRequest>("/course");
-group.MediatePost<PostQueryCourseRequest>("/course/query");
-group.MediatePost<CreateCourseRequest>("/course");
+group.MediateGet<GetCourseRequest>("/course/{courseId:guid}","Course",typeof(Course));
+group.MediateGet<GetAllCoursesRequest>("/course","Course",typeof(List<Course>));
+group.MediatePost<PostQueryCourseRequest>("/course/query","Course",typeof(List<Course>));
+group.MediatePost<CreateCourseRequest>("/course","Course",typeof(Course));
 
 app.Run();
