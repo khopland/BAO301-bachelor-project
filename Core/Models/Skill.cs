cfe13fe87351d9d7511d073a5a8ac72748d52ca6
Kristoffer Larsen Hopland <kristoffer.larsen.hopland@bos.no>
2023-03-31 11:16:47 +0200

    fix many-many relation and added seed data

--- Core/Models/Skill.cs
+++ Core/Models/Skill.cs
@@ -5,5 +5,6 @@ public class Skill
     public Guid Id { get; set; }
     public string Name { get; set; } = default!;
     public string? Description { get; set; }
+    public ICollection<Course> Courses { get; set; } = new List<Course>();
 
 }
