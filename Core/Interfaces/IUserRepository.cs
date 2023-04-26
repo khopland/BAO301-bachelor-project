d9e07370074cfe21fc006811b19b614bcecc8ed8
Kristoffer Larsen Hopland <24739853+BM0B0T@users.noreply.github.com>
2023-04-26 22:53:35 +0200

    added feature to complete a course

--- Core/Interfaces/IUserRepository.cs
+++ Core/Interfaces/IUserRepository.cs
@@ -8,5 +8,5 @@ public interface IUserRepository
     public Task<User> CreateUser(User user,CancellationToken cancellationToken);
     public Task<User?> GetUserById(Guid userId, CancellationToken cancellationToken);
     public Task AddEnrollmentToUser(User user,Course course, CancellationToken cancellationToken);
-    Task CompleteEnrollmentForUser(Guid enrollmentId, CancellationToken cancellationToken);
+    public Task CompleteEnrollmentForUser(Guid enrollmentId, CancellationToken cancellationToken);
 }
